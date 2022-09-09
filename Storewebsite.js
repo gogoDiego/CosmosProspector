bdc5a417ac4cd3b6a393b8d4bf7f2cf69c5db950
GogoDiego <83057973+gogoDiego@users.noreply.github.com>
2022-09-09 01:19:39 -0500

    Create Storewebsite.js

---
+++ Storewebsite.js
@@ -0,0 +1,250 @@
+
+
+
+
+const articles = [
+
+
+{
+title: "Osmosis",
+photo:"https://imgs.search.brave.com/KIchLk0I-pV2CTrUWc91XG6nD8dm5UT9CNHYfyaHiYg/rs:fit:1200:720:1/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS92/M2xJbnhfMTVoOC9t/YXhyZXNkZWZhdWx0/LmpwZw",
+logo:"https://imgs.search.brave.com/lrwoph-gkc9n5uSQ2uZSRnqafolTMvR-Bkhl1pFnirY/rs:fit:250:250:1/g:ce/aHR0cHM6Ly9zdGF0/aWMuY29pbmNvc3Qu/bmV0L2xvZ28vY3J5/cHRvY3VycmVuY3kv/b3Ntb3Npcy5wbmc",
+description: "The Premiere Interchain Dex/AMM of The Cosmos Ecosystem, Swap, LP, Stake, & more.",
+tags:["all","chain","dex","defi",],
+link:"https://app.osmosis.zone/",
+},
+
+{
+title:"Juno",
+photo:"https://user-images.githubusercontent.com/79812965/131373443-5ff0d9f6-2e2a-41bd-8347-22ac4983e625.jpg",
+logo:"https://2549163913-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2F-MZiFiIZH5IkEYsWMUO4%2Fuploads%2Fgit-blob-4ea6081652796a8c60be99667f61753ad9983ad2%2FJUNO%20Logo%20(Black).svg?alt=media",
+description:"Use & create interoperable applications on Juno. A decentralized, public, permission-less network for cross-chain smart contracts.",
+tags:["all","chain","smartcontracts"],
+link:"https://www.junonetwork.io/",
+},
+
+{
+
+title:"Cresent",
+photo:"https://static-resources.crescent.network/OG-landing.jpeg",
+logo:"https://2299351957-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FmEYtCuIUIqObZPWrSUQb%2Ficon%2FCv26i0d4sHliNOg5DlLt%2FProfile%20Logo%20Image%20.png?alt=media&token=76b124da-6df3-46af-a55a-2fce8ad97e75",
+description:"Innovating DeFi investment through advancements inter-blockchain technology",
+tags:["all","chain","dex","defi",],
+link:"https://app.crescent.network/orderbook",
+},
+
+{
+
+title:"Akash",
+photo:"https://imgs.search.brave.com/k4Op2fgz8QW7I9ppEM6FYu5VJYaFNY921WRFCxdqVek/rs:fit:750:440:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vZGFpbHlo/b2RsLmNvbS93cC1j/b250ZW50L3VwbG9h/ZHMvMjAyMS8wMS9u/ZXctZmF2b3JpdGUt/Y29pbi0xLmpwZz9y/ZXNpemU9NzUwJTJD/NDQwJnNzbD0x",
+logo:"https://imgs.search.brave.com/8pB38iFgwX9u8WHDH5J5R0vjrnboSUQmLE2QyKI60xw/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9jcnlw/dG9sb2dvcy5jYy9s/b2dvcy9ha2FzaC1u/ZXR3b3JrLWFrdC1s/b2dvLnBuZw",
+description:"Using Akash Network allows developers to launch applications such as DeFi apps, block explorers, and other blockchain network components on a decentralized cloud.",
+tags:["all","chain","tools",],
+link:"https://akash.network/",
+},
+{
+
+title:"Juno Swap",
+photo:"https://pbs.twimg.com/media/FJFkqoUWUAQqXUA.jpg:large",
+logo:"https://1347255254-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FkIBcvuSNFl67qnhKwSqU%2Fuploads%2FTNoZ3iET0TeAW0AnTq7V%2FJunoSwap%20Logo%20.svg?alt=media&token=a87f21d3-6aac-4221-97f1-dfb99e5d4fcc",
+description:"Inter-chain AMM. Trade, list and liquify Cosmos native or CW-20 assets. Built on JunoNetwork",
+tags:["all","dex","defi"],
+link:"https://junoswap.com/",
+},
+{
+
+title:"Secret Network",
+photo:"https://lh4.googleusercontent.com/h5bK8qfNE0zR7LpjcKVdMA2WhN2TkZd7m4kjZo8qygiLsBw-ZFg9nlG0b1o2YZHS7m11UKKjbCs-rjVcxP-Hp2EDK8Gwia8Q9x7t6fH3st-OnZC2tMe92KhCg0AY7HMllziBQxiG",
+logo:"https://ghost-zxtx.onrender.com/content/images/2022/05/Pictoral-Mark.png",
+description:"Secret Network is the first blockchain with customizable privacy. This protects users, and empowers developers to build a better Web3.",
+tags:["all","chain","smartcontracts",],
+link:"https://scrt.network/",
+},
+
+{
+
+title:"Stargaze",
+photo:"https://hiddengemteam.com/wp-content/uploads/2022/02/stargaze.jpg",
+logo:"https://s2.coinmarketcap.com/static/img/coins/200x200/16842.png",
+description:"A decentralized, community-owned NFT Layer 1 chain and marketplace with CosmWasm smart contracts. 100% carbon neutral. Zero gas.",
+tags:["all","chain","nfts",],
+link:"https://www.stargaze.zone/",
+},
+{
+
+    title:"Umee",
+    photo:"https://pbs.twimg.com/media/FbF_fIRUEAQrDss?format=jpg&name=large",
+    logo:"https://imgs.search.brave.com/iIq6clqLDbvmunJrebOsKveFJNh3EBZFGaESan91v-o/rs:fit:250:218:1/g:ce/aHR0cHM6Ly9zdGF0/aWMuY29pbmNvc3Qu/bmV0L2xvZ28vY3J5/cHRvY3VycmVuY3kv/dW1lZS5wbmc",
+    description:"Simplest way to start your DeFi experience for staking, rates, and interoperable solutions across blockchains. Borrow and lend today",
+    tags:["all","chain","defi"],
+    link:"https://umee.cc/",
+    },
+
+];
+
+
+
+const grid = document.getElementById("grid");
+
+
+
+function makeArticle (index){
+
+    let picker = (index);
+
+    const article = document.createElement("div");
+    article.className = "article";
+    grid.appendChild(article);
+    article.id = "article" + [picker];
+
+    const articlediv = document.getElementById( "article" + picker);
+
+    const photo = document.createElement("img");
+    photo.src = articles[picker].photo;
+    photo.className = "photo";
+    articlediv.appendChild(photo);
+
+    const articleCenter = document.createElement("div");
+    articleCenter.id = "articleCenter" + [picker];
+    articlediv.appendChild(articleCenter);
+
+    const articleCenterDiv = document.getElementById( "articleCenter" + picker);
+
+
+    const articlehead = document.createElement("div");
+    articlehead.className = "articlehead";
+    articlehead.id = "articlehead" + [picker];
+    articleCenterDiv.appendChild(articlehead);
+
+    const articleheaddiv = document.getElementById( "articlehead" + picker);
+
+    const title = document.createElement("h3");
+    title.textContent = articles[picker].title;
+    title.className = "title";
+    articleheaddiv.appendChild(title);
+
+    const logo = document.createElement("img");
+    logo.src = articles[picker].logo;
+    logo.className = "logo";
+    articleheaddiv.appendChild(logo);
+
+    const horizontalRule = document.createElement("hr");
+    horizontalRule.className = "horirule";
+    articleCenterDiv.appendChild(horizontalRule);
+
+    const description = document.createElement("p");
+    description.className = "description";
+    description.textContent = articles[picker].description;
+    articleCenterDiv.appendChild(description);
+
+
+}
+
+
+function links () {
+
+    let clickDiv = document.querySelectorAll(".article");
+
+    clickDiv.forEach( function (btns){
+    
+    
+        //this shows the individual event listener
+        btns.addEventListener("click", function (e) {
+    
+            //this makes the event into a simple variable to use
+            epicEvent = e.currentTarget; //or e.currentTarget.classList
+    
+            
+    
+            let articleLinkIndex = epicEvent.id.slice(7);
+    
+      
+            window.open(articles[articleLinkIndex].link, '_blank');
+    
+    });
+    
+    });
+    
+    }
+
+
+for (let i = 0; i < articles.length ; i++){
+
+    makeArticle(i)
+
+}
+
+
+
+const buttons = document.querySelectorAll(".btns");
+
+
+  
+links();
+
+
+
+
+
+buttons.forEach( function (btns){
+
+	btns.addEventListener("click", function (e) {
+
+		epicEvent = e.currentTarget; //or e.currentTarget.classList
+
+        grid.innerHTML = "";
+
+
+
+    for(let i = 0; i < articles.length ; i++){
+
+    let articleTags = articles[i].tags;
+
+
+    function tagsearch (item) {
+        
+        let target = epicEvent.classList[1];
+        return item == target;
+    }
+
+    let searchResults = articleTags.some(tagsearch);
+
+    if (searchResults == true){
+        
+        makeArticle(i)
+
+    }
+
+   
+    links();
+
+}
+
+
+
+    });
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
